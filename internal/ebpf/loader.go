a4cc279d2940d474d191562834424561d9b98a51
Hudson Coutinho <hudson.coutinho@evolutto.com>
2026-03-23 21:21:18 -0300

    fix: keep cilium/ebpf in go.sum; align Go 1.24

--- internal/ebpf/loader.go
+++ internal/ebpf/loader.go
@@ -1,5 +1,10 @@
 package ebpf
 
+import "github.com/cilium/ebpf"
+
+// Keeps github.com/cilium/ebpf in go.sum for bpf2go-generated code.
+var _ ebpf.Program
+
 type Loader struct{}
 
 func NewLoader() (*Loader, error) {
